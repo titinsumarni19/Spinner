88191a25736091452367ee69f78b8bb1bbca7670
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-05-08 10:05:21 +0700

    Update MainActivity.java

--- app/src/main/java/com/titinsumarni202102246/spinner/MainActivity.java
+++ app/src/main/java/com/titinsumarni202102246/spinner/MainActivity.java
@@ -10,25 +10,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner _spinner1, _spinner2, _spinner3, _spinner4;
+    private Spinner _spinner1;
+    private Spinner _spinner2;
+    private Spinner _spinner3;
+    private int bil;
+    private Spinner _spinner4;
+    private Spinner _spinner5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _spinner1 = (Spinner)findViewById(R.id.spinner1);
-        _spinner2 = (Spinner)findViewById(R.id.spinner2);
-        _spinner3 = (Spinner)findViewById(R.id.spinner3);
-        _spinner4 = (Spinner)findViewById(R.id.spinner4);
+        _spinner1 = (Spinner) findViewById(R.id.spinner1);
+        _spinner2 = (Spinner) findViewById(R.id.spinner2);
+        _spinner3 = (Spinner) findViewById(R.id.spinner3);
+        _spinner4 = (Spinner) findViewById(R.id.spinner4);
+        _spinner5 = (Spinner) findViewById(R.id.spinner5);
 
         initSpinner1();
         initSpinner2();
         initSpinner3();
         initSpinner4();
+        initSpinner5();
     }
 
-    private void initSpinner1(){
+    private void initSpinner1() {
         List<String> buahList = new ArrayList<>();
         buahList.add("Rambutan");
         buahList.add("Langsat");
@@ -36,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         buahList.add("Nangka");
         buahList.add("Mangga");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,buahList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, buahList);
         _spinner1.setAdapter(arrayAdapter);
     }
 
@@ -54,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
     private void initSpinner3() {
         List<String> ganjilList = new ArrayList<>();
 
-        for(int i = 1; i <= 999; i+=2){
+        for (int i = 1; i <= 999; i += 2) {
             ganjilList.add("Bilangan ke-" + i);
         }
 
@@ -62,10 +69,10 @@ public class MainActivity extends AppCompatActivity {
         _spinner3.setAdapter(arrayAdapter3);
     }
 
-    private  void initSpinner4(){
+    private void initSpinner4() {
         List<String> genapList = new ArrayList<>();
 
-        for(int i = 2; i <= 1000; i+=2){
+        for (int i = 2; i <= 1000; i += 2) {
             genapList.add("Bilangan ke-" + i);
         }
 
@@ -73,4 +80,24 @@ public class MainActivity extends AppCompatActivity {
         _spinner4.setAdapter(arrayAdapter4);
     }
 
+    private void initSpinner5() {
+        List<String> primaList = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i += 1) {
+            bil = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    bil = bil + 1;
+                }
+            }
+            if (bil == 2) {
+
+                primaList.add("Bilangan Prima ke-" + i);
+            }
+
+            ArrayAdapter<String> arrayAdapter5 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, primaList);
+            _spinner5.setAdapter(arrayAdapter5);
+        }
+
+    }
 }
